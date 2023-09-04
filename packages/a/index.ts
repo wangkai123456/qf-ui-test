f81fb2360755ebdf76b4b76bbdb7d06b2b0cbc18
wangkai <1205669217@qq.com>
2023-09-04 22:39:12 +0800

    init

---
+++ packages/a/index.ts
@@ -0,0 +1,3 @@
+export default () => {
+  console.log("你好，我是多包管理1111")
+}
---
+++ play/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    测试管用吗 {{ num }} 
+    <button @click="num += 1">+1</button>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+import a from '@qf/a'
+a()
+
+const num = ref(1)
+
+</script>
